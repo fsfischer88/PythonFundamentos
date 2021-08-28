92ee29862774c987b2ea3ee7b0b457fb066a45cf
Felipe S. Fischer <felipesfischer@gmail.com>
2021-08-28 18:16:08 -0300

    git commit -m Tupla
    git commit -m

---
+++ aula2/a2.3-2.py
@@ -0,0 +1,11 @@
+# Curso: Pyhton Fundamentos Proway
+# Aula: Dia 2 - Parte 3
+# Assunto: Tuplas
+# Data: 2021-08-28-m 
+
+# Cria uma lista inalteravel
+bebidas = ('Coca-Cola','Skol','Cachaça','Água') #Na Tuplas se utiliza (). 
+print(bebidas[0]) # imprimindo o primeiro elemento da tupla
+print(bebidas[2:]) # imprimindo parte da tupla
+nome = 'Felipe'
+print(nome[1])
