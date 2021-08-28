c70c09a2592c3cde40eefa12b19ec40b7d73928e
Felipe S. Fischer <felipesfischer@gmail.com>
2021-08-28 17:21:03 -0300

    Aula 2 - parte 3

---
+++ aula2/a2.3.py
@@ -0,0 +1,14 @@
+# Curso: Pyhton Fundamentos Proway
+# Aula: Dia 2 - Parte 3
+# Assunto: Listas, Tuplas, Dicionários
+# Data: 2021-08-28-m 'Aula2 - parte 2 - While'
+
+bebidas = ['Coca-Cola','Skol','Cachaça','Água']
+print(bebidas)
+print( bebidas[1] ) # segundo elemento da lista
+print( bebidas[3] ) # quarto elemento da lista
+print( bebidas[1:] ) # do segundo elemento até o final da lista
+print( bebidas[:3] ) # do inicio ate o terceiro elemento da lista
+
+bebidas.append('Corona')
+print(bebidas)
