1956ffaa0a1838ff350079f14a84e0a013107108
Felipe S. Fischer <felipesfischer@gmail.com>
2021-08-28 17:47:09 -0300

    Aula 2 - parte 3

--- aula2/a2.3.py
+++ aula2/a2.3.py
@@ -12,3 +12,20 @@ print( bebidas[:3] ) # do inicio ate o terceiro elemento da lista
 
 bebidas.append('Corona')
 print(bebidas)
+bebidas[3] = 'Vodka'
+print(bebidas)
+bebidas.remove('Skol')
+print(bebidas)
+bebidas.pop(-1) # deleta o último elemento
+print(bebidas)
+ultima_bebida = bebidas.pop(-1)
+print(ultima_bebida)
+print(bebidas)
+#inserir
+bebidas.insert(0,'Refri') # Serve para inserir um elemento em uma determinada posição
+bebidas.insert(2,'Cafe')
+print(bebidas)
+# deleta um dado ou intervalo de dados de uma lista
+del(bebidas[1:3])
+print(bebidas)
+
