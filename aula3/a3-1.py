d8ad9d9cb974bcb3fccd430eccd940c99291b8cd
Felipe S. Fischer <felipesfischer@gmail.com>
2021-09-11 14:20:38 -0300

    Aula3 - Procedimentos

---
+++ aula3/a3-1.py
@@ -0,0 +1,34 @@
+# Curso: Pyhton Fundamentos Proway
+# Aula: Dia 3 - Parte 3 - 1
+# Assunto: Funções
+# Data: 2021-09-11
+
+# Procedimento: Não possui retorno
+# Função: Existe retorno
+# paramento: é a variavél recebida em uma função ou procedimento
+# argumento: é o dado passado durante a chamada de uma função ou procedimento
+
+def somar(numero1, numero2): # Parametro
+    resultado = numero1 + numero2
+    print(resultado)
+
+somar(10, 12) # Argumento
+
+def diminuir(numero1, numero2):
+    resultado = numero1 - numero2
+    print(resultado)
+
+diminuir(10, 12)
+
+
+def dividir(numero1, numero2):
+    resultado = numero1 / numero2
+    print(resultado)
+
+dividir(10, 5)
+
+def multiplicar(numero1, numero2):
+    resultado = numero1 * numero2
+    print(resultado)
+
+multiplicar(10, 5)
